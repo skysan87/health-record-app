d3f2b60429ca0a507849650f77d40854f4479f10
skysan <26142873+skysan87@users.noreply.github.com>
2023-03-19 15:44:56 +0900

    コンポーネント切り出し

---
+++ src/components/ActivityRecord.vue
@@ -0,0 +1,153 @@
+<template>
+  <div>
+    <div class="flex flex-row">
+      <div class="flex-1">
+        <span>運動メニュー</span>
+        <div v-for="m in activityMenu" :key="m.label">
+          <label class="ml-2 align-middle">
+            <input v-model="selectedActivity" type="radio" :value="m" @change="onChangeActivity">
+            <span>{{ m.label }}</span>
+          </label>
+        </div>
+        <div class="flex items-center">
+          <label class="ml-2 align-middle">
+            <input v-model="selectedActivity" type="radio" :value="activityOther" @change="onChangeActivity">
+            <span>{{ activityOther.label }}</span>
+          </label>
+          <input type="text" class="ml-2 input-text" style="width: fit-content;">
+        </div>
+      </div>
+      <div class="flex-none">
+        <button class="m-1 h-8 w-8 btn btn-outline" @click="openDialog">
+          <fa :icon="['fas', 'edit']" size="sm" />
+        </button>
+      </div>
+    </div>
+
+    <div class="border-b pt-2" />
+
+    <div class="pt-2">
+      <div class="pb-2">
+        <div class="flex items-center">
+          <span>実施単位</span>
+          <span
+            v-if="selectedActivity?.unit"
+            class="ml-1 badge bg-blue-200"
+          >
+            {{ selectedActivity?.value }}kcal / {{ selectedActivity?.unit }}
+          </span>
+        </div>
+        <input
+          v-model="valueUnit"
+          type="number"
+          inputmode="decimal"
+          class="input-text"
+          placeholder="実装した回数を追加"
+          @input="calcKcal"
+        >
+      </div>
+      <div>
+        <span>消費エネルギー</span>
+        <span class="output-text">{{ valueKcal ?? 0 }}</span>
+      </div>
+
+      <div class="pt-2">
+        <button class="btn btn-regular" @click="recordActivity">
+          登録
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import ActivityMenuDialog from '@/components/ActivityMenuDialog'
+
+const DialogController = Vue.extend(ActivityMenuDialog)
+
+const activityOther = { label: 'その他', value: 1, unit: '' }
+
+export default {
+
+  data () {
+    return {
+      activityOther,
+      selectedActivity: null,
+      valueKcal: null,
+      valueUnit: null,
+      dialog: null
+    }
+  },
+
+  computed: {
+    activityMenu () {
+      return this.$store.getters['Activity/getMenu']
+    }
+  },
+
+  methods: {
+    onChangeActivity () {
+      if (!this.selectedActivity) {
+        return
+      }
+      this.valueUnit = null
+      this.valueKcal = null
+    },
+
+    calcKcal () {
+      if (!this.selectedActivity) {
+        return
+      }
+      const data = this.valueUnit * this.selectedActivity.value
+      this.valueKcal = parseFloat(data.toFixed(2))
+    },
+
+    openDialog () {
+      delete this.dialog
+      this.dialog = new DialogController({
+        propsData: {
+          parent: this.$root.$el,
+          target: this.activityMenu
+        }
+      })
+      this.dialog.$on('update', (result) => {
+        this.$store.dispatch('Activity/updateMenu', result)
+          .then(() => {
+            this.clearActivityInput()
+          })
+          .catch((error) => {
+            console.error(error)
+            this.$toast.error('更新に失敗しました')
+          })
+      })
+      this.dialog.$mount()
+    },
+
+    recordActivity () {
+      if (!this.selectedActivity) {
+        return
+      }
+      this.$store.dispatch('Activity/addRecord', {
+        timestamp: new Date(),
+        name: this.selectedActivity.label,
+        value: this.valueKcal
+      })
+        .then(() => {
+          this.clearActivityInput()
+        })
+        .catch((error) => {
+          console.error(error)
+          this.$toast.error('登録に失敗しました')
+        })
+    },
+
+    clearActivityInput () {
+      this.selectedActivity = null
+      this.valueKcal = null
+      this.valueUnit = null
+    }
+
+  }
+}
+</script>
--- src/pages/healthcare.vue
+++ src/pages/healthcare.vue
@@ -14,7 +14,7 @@
     <!-- ラジオボタンで表示切り替え -->
     <div class="flex-1 flex flex-row pt-2 px-2">
       <label v-for="m in menu" :key="m.value" class="ml-2 align-middle">
-        <input v-model="selectedMenu" type="radio" :value="m" @change="changeMenu">
+        <input v-model="selectedMenu" type="radio" :value="m">
         <span>{{ m.label }}</span>
       </label>
     </div>
@@ -71,72 +71,16 @@
     </div>
 
     <!-- 運動記録 -->
-    <div v-show="selectedMenu === menu.Activity" class="pt-2 px-2">
-      <div class="flex flex-row">
-        <div class="flex-1">
-          <span>運動メニュー</span>
-          <div v-for="m in activityMenu" :key="m.label">
-            <label class="ml-2 align-middle">
-              <input v-model="selectedActivity" type="radio" :value="m" @change="onChangeActivity">
-              <span>{{ m.label }}</span>
-            </label>
-          </div>
-          <div class="flex items-center">
-            <label class="ml-2 align-middle">
-              <input v-model="selectedActivity" type="radio" :value="activityOther" @change="onChangeActivity">
-              <span>{{ activityOther.label }}</span>
-            </label>
-            <input type="text" class="ml-2 input-text" style="width: fit-content;">
-          </div>
-        </div>
-        <div class="flex-none">
-          <button class="m-1 h-8 w-8 btn btn-outline" @click="openDialog">
-            <fa :icon="['fas', 'edit']" size="sm" />
-          </button>
-        </div>
-      </div>
-
-      <div class="border-b pt-2" />
-
-      <div class="pt-2">
-        <div class="pb-2">
-          <div class="flex items-center">
-            <span>実施単位</span>
-            <span v-if="selectedActivity?.unit" class="ml-1 badge bg-blue-200">{{ selectedActivity?.value }}kcal / {{ selectedActivity?.unit }}</span>
-          </div>
-          <input
-            v-model="valueUnit"
-            type="number"
-            inputmode="decimal"
-            class="input-text"
-            placeholder="実装した回数を追加"
-            @input="calcKcal"
-          >
-        </div>
-        <div>
-          <span>消費エネルギー</span>
-          <span class="output-text">{{ valueKcal ?? 0 }}</span>
-        </div>
-
-        <div class="pt-2">
-          <button class="btn btn-regular" @click="recordActivity">
-            登録
-          </button>
-        </div>
-      </div>
-    </div>
+    <activity-record v-show="selectedMenu === menu.Activity" class="pt-2 px-2" />
   </div>
 </template>
 
 <script>
-import Vue from 'vue'
-import ActivityMenuDialog from '@/components/ActivityMenuDialog'
 import { Health } from '@/model/Health'
 import { Healthlist } from '@/model/Healthlist'
 import { fixFloat } from '@/util/NumberUtil'
 import CommandableInput from '@/components/parts/CommandableInput'
-
-const DialogController = Vue.extend(ActivityMenuDialog)
+import ActivityRecord from '@/components/ActivityRecord'
 
 const menu = {
   Activity: { label: '運動', value: 'activity' },
@@ -144,14 +88,11 @@ const menu = {
   Goal: { label: '目標値', value: 'goal' }
 }
 
-const healthType = { weight: Health.TYPE_WEIGHT, height: Health.TYPE_HEIGHT }
-
-const activityOther = { label: 'その他', value: 1, unit: '' }
-
 export default {
 
   components: {
-    CommandableInput
+    CommandableInput,
+    ActivityRecord
   },
 
   layout: ctx => ctx.$device.isMobile ? 'board_mobile' : 'board',
@@ -159,14 +100,7 @@ export default {
   data () {
     return {
       menu,
-      healthType,
-      selectedMenu: menu.Activity,
-      activityOther,
-      selectedActivity: null,
-      valueKcal: null,
-      valueUnit: null,
-      valueHealth: null,
-      dialog: null
+      selectedMenu: menu.Activity
     }
   },
 
@@ -177,10 +111,6 @@ export default {
 
     BMI () {
       return this.$store.getters['Health/calcBMI']
-    },
-
-    activityMenu () {
-      return this.$store.getters['Activity/getMenu']
     },
 
     totalCalorie () {
@@ -212,49 +142,6 @@ export default {
         console.error(error)
         this.$toast.error('初期化に失敗しました')
       }
-    },
-
-    changeMenu () {
-      this.valueUnit = null
-      this.valueKcal = null
-      this.valueHealth = null
-    },
-
-    onChangeActivity () {
-      if (!this.selectedActivity) {
-        return
-      }
-      this.valueUnit = null
-      this.valueKcal = null
-    },
-
-    calcKcal () {
-      if (!this.selectedActivity) {
-        return
-      }
-      const data = this.valueUnit * this.selectedActivity.value
-      this.valueKcal = parseFloat(data.toFixed(2))
-    },
-
-    openDialog () {
-      delete this.dialog
-      this.dialog = new DialogController({
-        propsData: {
-          parent: this.$root.$el,
-          target: this.activityMenu
-        }
-      })
-      this.dialog.$on('update', (result) => {
-        this.$store.dispatch('Activity/updateMenu', result)
-          .then(() => {
-            this.clearActivityInput()
-          })
-          .catch((error) => {
-            console.error(error)
-            this.$toast.error('更新に失敗しました')
-          })
-      })
-      this.dialog.$mount()
     },
 
     // コールバック処理
@@ -327,34 +214,6 @@ export default {
         return false
       }
       return true
-    },
-
-    recordActivity () {
-      if (this.selectedMenu !== menu.Activity) {
-        return
-      }
-
-      if (!this.selectedActivity) {
-        return
-      }
-
-      this.$store.dispatch('Activity/addRecord', {
-        timestamp: new Date(),
-        name: this.selectedActivity.label,
-        value: this.valueKcal
-      })
-        .then(() => {
-          this.clearActivityInput()
-        })
-        .catch((error) => {
-          console.error(error)
-          this.$toast.error('登録に失敗しました')
-        })
-    },
-    clearActivityInput () {
-      this.selectedActivity = null
-      this.valueKcal = null
-      this.valueUnit = null
     }
   }
 }
