4abe60dc7924cf27c0d85f191b9db4b0a2ed8fe9
skysan <26142873+skysan87@users.noreply.github.com>
2023-04-03 23:10:02 +0900

    タイムラインチャート実装

---
+++ src/components/charts/TimelineChart.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="chart" />
+</template>
+
+<script>
+import 'chartist/dist/index.css'
+import { LineChart, FixedScaleAxis } from 'chartist'
+
+export default {
+  name: 'TimelineChart',
+
+  data () {
+    return {
+      chart: null
+    }
+  },
+
+  mounted () {
+  },
+
+  methods: {
+    init (series) {
+      this.chart = new LineChart(
+        '#chart',
+        {
+          series
+        },
+        {
+          axisX: {
+            type: FixedScaleAxis,
+            divisor: 5,
+            labelInterpolationFnc: value =>
+              new Date(value).toLocaleString(undefined, {
+                year: '2-digit',
+                month: 'short',
+                day: 'numeric'
+              })
+          }
+        }
+      )
+    },
+
+    update (series) {
+      this.chart.update({ series })
+    }
+  }
+}
+</script>
