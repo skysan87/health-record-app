6443ecfa82687d7f2311664f9611b9f6274e8e4b
skysan <26142873+skysan87@users.noreply.github.com>
2023-06-26 17:55:47 +0900

    nuxtプロジェクト構成変更

--- apps/v2/nuxt.config.ts
+++ apps/v2/nuxt.config.ts
@@ -1,4 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  srcDir: 'src',
+  ssr: false,
+  postcss: {
+    plugins: {
+      tailwindcss: {}
+    }
+  },
+  modules: [
+  ],
+  dir: {
+    layouts: "layouts",
+    pages: "pages",
+    assets: "assets",
+    public: "public",
+    static: "public",
+    middleware: "middleware",
+    modules: "modules",
+    plugins: "plugins",
+  },
+  css: [
+    '@/assets/css/tailwind.css',
+    '@/assets/css/common.css',
+    '@fortawesome/fontawesome-svg-core/styles.css'
+  ],
+  typescript: {
+    strict: false
+  }
 })
---
+++ apps/v2/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    `./src/components/**/*.{vue,js,ts}`,
+    `./src/layouts/**/*.vue`,
+    `./src/pages/**/*.vue`,
+    `./src/composables/**/*.{js,ts}`,
+    `./src/plugins/**/*.{js,ts}`,
+    `./src/utils/**/*.{js,ts}`,
+    `./src/App.{vue,js,ts}`,
+    `./src/app.{vue,js,ts}`,
+    `./src/Error.{vue,js,ts}`,
+    `./src/error.{vue,js,ts}`,
+    `./src/app.config.{js,ts}`,
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
