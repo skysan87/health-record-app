8f08b0533eda39c144992021def3e02811905bc5
skysan <26142873+skysan87@users.noreply.github.com>
2023-07-02 19:52:14 +0900

    運動登録画面実装

---
+++ apps/v2/src/composables/useMenu.ts
@@ -0,0 +1,34 @@
+import { Menu } from "@health-record/core/value-object"
+import { fixFloat } from '@health-record/core/util/NumberUtil'
+
+export const useMenu = () => {
+  const input = ref<Menu[]>([])
+
+  return {
+    list: input,
+    init: (value: readonly Menu[]): void => {
+      input.value = [...value] // copy
+    },
+    updateRows: (): Menu[] => {
+      return input.value
+        .filter((a: Menu) => a.label !== null && a.label !== '')
+        .map((a: Menu) => {
+          return {
+            label: a.label.trim(),
+            value: fixFloat(a.value, 1),
+            unit: a.unit ? a.unit?.trim() : ''
+          } as Menu
+        })
+    },
+    deleteRow: (index: number) => {
+      input.value.splice(index, 1)
+    },
+    addRow: () => {
+      input.value.push({
+        label: null,
+        value: null,
+        unit: null
+      } as Menu)
+    }
+  }
+}
---
+++ apps/v2/src/pages/healthcare.vue
@@ -0,0 +1,104 @@
+<script setup lang="ts">
+import { useActivity } from '@/composables/useActivity'
+import { useHealth } from '@/composables/useHealth'
+
+const { initActivity, totalCalorie } = useActivity()
+const { latestData, isOutOfLineBMI, BMI, notAchievedGoal, initHealth, recordWeight, recordHeight, setGoalActivity, setGoalWeight } = useHealth()
+
+const menu = {
+  Activity: { label: '運動', value: 'activity' },
+  Health: { label: '測定値', value: 'health' },
+  Goal: { label: '目標値', value: 'goal' }
+} as const
+type menu = typeof menu[keyof typeof menu]
+
+const selectedMenu = ref<menu>(menu.Activity)
+
+// TODO: mobile対応
+definePageMeta({
+  layout: 'board'
+})
+
+const init = async () => {
+  await Promise.all([initActivity(), initHealth()])
+}
+
+onBeforeMount(async () => await init())
+</script>
+
+<template>
+  <div class="flex flex-col h-full">
+    <div class="flex-none pt-2 px-2 flex flex-wrap">
+      <span class="ml-2">
+        <!-- <button title="リロード" @click="init">
+          <fa :icon="['fas', 'sync-alt']" size="lg" />
+        </button> -->
+      </span>
+      <span class="ml-2">●運動 <span :class="{'text-red-500': notAchievedGoal}">{{ totalCalorie }}kcal</span></span>
+      <span class="ml-2">●体重 {{ latestData?.weight }}kg</span>
+      <span class="ml-2">●BMI <span :class="{'text-red-500': isOutOfLineBMI}">{{ BMI }}</span></span>
+    </div>
+
+    <!-- ラジオボタンで表示切り替え -->
+    <div class="flex-none flex flex-row pt-2 px-2">
+      <label v-for="m in menu" :key="m.value" class="ml-2 align-middle">
+        <input v-model="selectedMenu" type="radio" :value="m">
+        <span>{{ m.label }}</span>
+      </label>
+    </div>
+
+    <div class="border-b pt-2" />
+
+    <div class="flex-1 overflow-y-auto pb-6">
+      <!-- 健康記録 -->
+      <div v-if="selectedMenu.value === menu.Health.value" class="pt-2 px-2">
+        <div class="pb-1 flex items-center">
+          <span class="p-2 w-1/4">体重(kg)</span>
+          <PartCommandableInput
+            key="latest-waight"
+            input-type="number"
+            :value="latestData?.weight"
+            :update="recordWeight"
+            inputmode="decimal" />
+        </div>
+        <div class="pb-1 flex items-center">
+          <span class="p-2 w-1/4">身長(cm)</span>
+          <PartCommandableInput
+            key="latest-height"
+            input-type="number"
+            :value="latestData?.height"
+            :update="recordHeight"
+            inputmode="decimal"
+          />
+        </div>
+      </div>
+
+      <!-- 目標設定 -->
+      <div v-else-if="selectedMenu.value === menu.Goal.value" class="pt-2 px-2">
+        <div class="pb-1 flex items-center">
+          <span class="p-2 w-1/4">運動量(kcal)</span>
+          <PartCommandableInput
+            key="goal-activity"
+            input-type="number"
+            :value="goal?.activity"
+            :update="setGoalActivity"
+            inputmode="decimal"
+          />
+        </div>
+        <div class="pb-1 flex items-center">
+          <span class="p-2 w-1/4">体重(kg)</span>
+          <PartCommandableInput
+            key="goal-weight"
+            input-type="number"
+            :value="goal?.weight"
+            :update="setGoalWeight"
+            inputmode="decimal"
+          />
+        </div>
+      </div>
+
+      <!-- 運動記録 -->
+      <ActivityRecord v-else-if="selectedMenu.value === menu.Activity.value" class="pt-2 px-2" />
+    </div>
+  </div>
+</template>
