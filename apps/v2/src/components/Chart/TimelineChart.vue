be1e9d4910da9c256db6001c64dc052d72819518
skysan <26142873+skysan87@users.noreply.github.com>
2023-07-05 01:08:30 +0900

    CompositionAPIに書き換え

--- apps/v2/src/components/Chart/TimelineChart.vue
+++ apps/v2/src/components/Chart/TimelineChart.vue
@@ -1,56 +1,47 @@
-<template>
-  <div id="chart" />
-</template>
-
-<script>
+<script setup lang="ts">
 import 'chartist/dist/index.css'
 import { LineChart, FixedScaleAxis, Interpolation } from 'chartist'
 import { dateFactory } from '@health-record/core/util/DateUtil'
 
-export default {
-  name: 'TimelineChart',
+let chart: LineChart = null
 
-  data () {
-    return {
-      chart: null
+const init = (series) => {
+  const serirsOptions = {}
+  series.forEach((s) => {
+    serirsOptions[`${s.name}`] = {
+      lineSmooth: Interpolation.cardinal({ tension: 0.1 }),
+      showPoint: false
     }
-  },
+  })
 
-  mounted () {
-  },
-
-  methods: {
-    init (series) {
-      const serirsOptions = {}
-      series.forEach((s) => {
-        serirsOptions[`${s.name}`] = {
-          lineSmooth: Interpolation.cardinal({ tension: 0.1 }),
-          showPoint: false
-        }
-      })
-
-      this.chart = new LineChart(
-        '#chart',
-        {
-          series
-        },
-        {
-          axisX: {
-            type: FixedScaleAxis,
-            divisor: 5,
-            labelInterpolationFnc: value =>
-              dateFactory(value).format('YY/MM/DD')
-          },
-          series: {
-            ...serirsOptions
-          }
-        }
-      )
+  chart = new LineChart(
+    '#chart',
+    {
+      series
     },
-
-    update (series) {
-      this.chart.update({ series })
+    {
+      axisX: {
+        type: FixedScaleAxis,
+        divisor: 5,
+        labelInterpolationFnc: value =>
+          dateFactory(value).format('YY/MM/DD')
+      },
+      series: {
+        ...serirsOptions
+      }
     }
-  }
+  )
 }
+
+const update = (series) => {
+  chart.update({ series })
+}
+
+defineExpose({
+  init, update
+})
 </script>
+
+<template>
+  <div id="chart" />
+</template>
