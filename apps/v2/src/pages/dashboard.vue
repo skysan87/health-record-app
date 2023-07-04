96b6ab021995501b2bc55ea22ede6ec761415d5f
skysan <26142873+skysan87@users.noreply.github.com>
2023-07-05 00:55:19 +0900

    グラフ機能追加

--- apps/v2/src/pages/dashboard.vue
+++ apps/v2/src/pages/dashboard.vue
@@ -1,7 +1,48 @@
 <script setup lang="ts">
+import { useChart } from '~/composables/useChart'
+
+const { chart, selectedRange, init, viewReset, viewPreview, viewNext } = useChart()
+
+const range: Array<{ label: string, value: number }> = [
+  { label: '2週', value: 14 },
+  { label: '1ヶ月', value: 30 },
+  { label: '2ヶ月', value: 60 },
+  { label: '3ヶ月', value: 90 },
+  { label: '6ヶ月', value: 180 }
+]
+
+// TODO: スマホ版メニュー表示中はグラフを非表示にする
+const isMenuExpanded = false
+
+watch(selectedRange, viewReset)
+
+onMounted(async () => await init())
+
 definePageMeta({
   layout: 'board'
 })
 </script>
 
-<template></template>
+<template>
+  <div v-show="!isMenuExpanded">
+    <header class="border-b flex">
+      <div class="cursor-pointer py-2 pl-4 inline-block text-gray-600" @click="viewPreview">
+        <fa :icon="['fas', 'arrow-left']" size="lg" ontouchend="" />
+      </div>
+      <div class="cursor-pointer py-2 pl-4 inline-block text-gray-600" @click="viewReset">
+        <fa :icon="['fas', 'house']" size="lg" ontouchend="" />
+      </div>
+      <div class="cursor-pointer py-2 pl-4 inline-block text-gray-600" @click="viewNext">
+        <fa :icon="['fas', 'arrow-right']" size="lg" ontouchend="" />
+      </div>
+      <div class="ml-auto py-2 px-4">
+        <select v-model="selectedRange" class="input-text">
+          <option v-for="r in range" :key="r.value" :value="r.value">
+            {{ r.label }}
+          </option>
+        </select>
+      </div>
+    </header>
+    <ChartTimelineChart ref="chart" />
+  </div>
+</template>
