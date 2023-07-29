acd6f45cf30e19bcda8a83ebf7f6bb2b331e97d4
skysan <26142873+skysan87@users.noreply.github.com>
2023-07-29 22:09:24 +0900

    optionからcomposition apiに書き換え

--- apps/v2/src/components/Part/IconButton.vue
+++ apps/v2/src/components/Part/IconButton.vue
@@ -1,17 +1,16 @@
+<script setup lang="ts">
+
+interface Props {
+  bgColor?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  bgColor: 'bg-gray-200'
+})
+</script>
+
 <template>
-  <div :class="`py-0 px-2 cursor-pointer rounded-full hover:${bgColor}`">
+  <div :class="`py-0 px-2 cursor-pointer rounded-full hover:${props.bgColor}`">
     <slot />
   </div>
 </template>
-
-<script>
-export default {
-  props: {
-    bgColor: {
-      type: String,
-      require: false,
-      default: 'bg-gray-200'
-    }
-  }
-}
-</script>
