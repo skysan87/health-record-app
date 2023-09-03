5d3364b99aa8140884881b4c36904ef75d4ca6c5
skysan <26142873+skysan87@users.noreply.github.com>
2023-09-03 12:49:35 +0900

    v1から目標体重の期間設定機能を移植

---
+++ apps/v2/src/plugins/v-calendar.client.ts
@@ -0,0 +1,13 @@
+import { setupCalendar } from 'v-calendar'
+import 'v-calendar/style.css'
+
+// グローバルコンポーネントに登録
+export default defineNuxtPlugin((nuxtApp) => {
+  // https://vcalendar.io/calendar/api.html#defaults
+  nuxtApp.vueApp.use(setupCalendar, {
+    masks: {
+      input: ['YYYY/MM/DD', 'L'],
+      title: 'YYYY MMMM'
+    }
+  })
+})
