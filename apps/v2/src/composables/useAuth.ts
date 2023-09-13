00917189aae37ae63bbb1fc6c0b0229d8241bfc0
skysan <26142873+skysan87@users.noreply.github.com>
2023-09-14 00:09:39 +0900

    ユーザ名の取得をメソッドに変更。 理由：composablesのスコープ上、boardで読み込まれると初期化されるため。 現在middlewareで同じcomposableを利用しており初期化に失敗するため、store化しない。

--- apps/v2/src/composables/useAuth.ts
+++ apps/v2/src/composables/useAuth.ts
@@ -5,13 +5,11 @@ import { AuthenticateUseCase } from "@health-record/core/usecase"
 export const useAuth = () => {
   const { $auth } = useNuxtApp()
   const usecase: AuthenticateUseCase = $auth()
-  const userInfo = ref<User>()
 
   return {
     login: async (onSuccess = (credential: User) => { }, onError = (error: NuxtError) => { }) => {
       try {
         const user = await usecase.login()
-        userInfo.value = user
         onSuccess(user)
       } catch (error) {
         onError(createError(error as NuxtError))
@@ -28,14 +26,14 @@ export const useAuth = () => {
     logout: async (onSuccess = () => { }, onError = (error: NuxtError) => { }) => {
       try {
         await usecase.logout()
-        userInfo.value = {} as User
         onSuccess()
       } catch (error) {
         onError(createError(error as NuxtError))
       }
     },
-    userName: computed<string>(() => {
-      return userInfo.value?.displayName ?? ''
-    })
+    getUserName: async () => {
+      const user = await usecase.getUser()
+      return user.displayName ?? ''
+    }
   }
 }
